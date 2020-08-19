3e7f89f6ca3b475c2f3f1c0a4b3470b33a48b4a0
cornwalljeremy <cornwall.jeremy@gmail.com>
2020-08-19 15:13:01 -0600

    quiz-layout

---
+++ assets/quizcode.js
@@ -0,0 +1,10 @@
+var startButtonEl = document.querySelector('#start');
+
+startButtonEl.addEventListener('click', function() {
+   startButtonEl.style.display = "none";
+});
+// array of objects 
+var questionsArray = [{
+    question1:  
+},{},{},{}] 
+
