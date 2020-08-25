6616b0dd087604fb6644bfda9f778987481c05d0
cornwalljeremy <cornwall.jeremy@gmail.com>
2020-08-25 15:04:00 -0600

    just about there

--- assets/quizcode.js
+++ assets/quizcode.js
@@ -1,16 +1,19 @@
 let timerEl = document.getElementById("countdown");
 let mainEl = document.getElementById("main");
-let startBtn = document.getElementById("start", "quiz");
-let startScreenEl = document.getElementById("start");
-let startQuestionsEl = document.getElementById("questions-start");
+let startBtn = document.getElementById("btn", "quiz-start");
+// let startScreenEl = document.getElementById("start");
+let quizStartEl = document.getElementById("quiz-start");
 let startInstructionsEl = document.getElementById("instructions");
 let hiddenItems = document.getElementsByClassName("hide");
-let questionList = document.getElementById("question-list");
+// let questionList = document.getElementById("question-list");
 let rightAnswer = 0;
+let questionIndex = 0
+let timeLeft = 3;
+let timeInterval;
 
 function countdown() {
-  let timeLeft = 500;
-  let timeInterval = setInterval(function () {
+  
+   timeInterval = setInterval(function () {
     if (timeLeft > 0) {
       timerEl.textContent = timeLeft;
       timeLeft--;
@@ -23,79 +26,56 @@ function countdown() {
       displayMessage();
     }
   }, 1000);
+  quiz()
 }
 
 function displayMessage() {
-  let wordCount = 0;
-  //   if(timeLeft < 0)
+  quizStartEl.innerHTML = ""
+  quizStartEl.textContent = "Your Score " + rightAnswer
+
 }
 
 function quiz() {
-  countdown();
+  // countdown();
 
   //   hiddenItems.removeAttribute("hide");
   startInstructionsEl.setAttribute("class", "hide");
-  startScreenEl.removeAttribute("class");
-  startScreenEl.setAttribute("class", "show");
+  // startScreenEl.removeAttribute("class");
+  // startScreenEl.setAttribute("class", "show");
 
-  startScreenEl.innerHTML = "<br>";
-  startScreenEl.innerHTML += questionsArray[0].q;
+  quizStartEl.innerHTML = "<br>";
+  quizStartEl.innerHTML += questionsArray[questionIndex].q;
 
-  function displayQuestion() {}
-  for (var i = 0; i < questionsArray[0].answers.length; i++) {
+  // function displayQuestion() {}
+  for (var i = 0; i < questionsArray[questionIndex].answers.length; i++) {
     var button = document.createElement("button");
     button.setAttribute("id", i);
     button.addEventListener("click", function (event) {
-      if (event.target.id === questionsArray[0].correctAnswer) {
+      if (event.target.id === questionsArray[questionIndex].correctAnswer) {
         rightAnswer = rightAnswer + 1;
       } else {
         rightAnswer = rightAnswer - 1;
         console.log(rightAnswer)
-        return displayQuestion;
+        timeLeft -= 10
+        // return displayQuestion;
       }
+      questionIndex++
+      if(questionsArray[questionIndex]){
+        quiz()
+      } else {
+          clearInterval(timeInterval)
+          displayMessage()
+
+      }
+      
+
     });
-    button.textContent = questionsArray[0].answers[i];
-    questionList.appendChild(button);
+    button.textContent = questionsArray[questionIndex].answers[i];
+    var stop = document.createElement("br")
+    quizStartEl.append(button);
 
   }
 
-  // let output = [];
-  // let answers;
-  // questionsArray.textContent = [count].length;
-  // i++;
-  //   for (i = 0; i < questionsArray.length; i++) let A = displayMessage(questionsArray[i].q);
-  //    if(
-  //     (A === "a" && questions[i].correctAnswer === "a") ||
-  //     (A === "b" && questions[i].correctAnswer === "b") ||
-  //     (A === "c" && questions[i].correctAnswer === "c") ||
-  //     (A === "d" && questions[i].correctAnswer === "d") ||
-  //     )
-}
-
-// score++
-// alert('correct');
-// } else {
-//     alert('wrong');
-//     timeLeft -= 10;
-// }
-
-// function sendScore({
-//     let scoreInitials = document.getElementById('initialEl')
-// })
-
-// function highScore(highScores) {
-//     HTMLTableSectionElement.textContent = "High Scores";
-
-//     displayQuestion.innerHTML = "<ul></ul>";
-//     displayQuestion.className = "score-item";
-//     for(let i = 0; i < highScores.length; i++){
-//         displayQuestion.appendChild = document.createElement('li');
-//     }
-// }
-// answer = document.querySelector("#answer")
-// answer.addEventListener('click',control);
-
-let count = 0;
 
 let questionsArray = [
   {
@@ -105,43 +85,43 @@ let questionsArray = [
   },
   {
     q: "What is Java?",
-    answers: {
-      a: "a high level programming language",
-      b: "a night club to buy drugs",
-      c: "good coffee",
-      d: "both a and c",
-    },
-    correctAnswer: "d",
+    answers: 
+      [ "a high level programming language",
+       "a night club to buy drugs",
+       "good coffee",
+       "both a and c"]
+    ,
+    correctAnswer: "3",
   },
   {
     q: "What does DOM stand for?",
-    answers: {
-      a: "Do or make",
-      b: "do over man",
-      c: "Document object model",
-      d: "dead on marriage..?",
-    },
-    correctAnswer: "c",
+    answers: 
+     [  "Do or make",
+       "do over man",
+       "Document object model",
+       "dead on marriage..?", ],
+    
+    correctAnswer: "2",
   },
   {
     q: "What is the answser to everything?",
-    answers: {
-      a: "coffee",
-      b: "puppies",
-      c: "'42'",
-      d: "all the above",
-    },
-    correctAnswer: "d",
+    answers: [
+       "coffee",
+       "puppies",
+       "'42'",
+       "all the above",
+  ],
+    correctAnswer: "3",
   },
   {
     q: "what does console.log do?",
-    answers: {
-      a: "solves world hunger",
-      b: "give the cheat codes to your fav video game",
-      c: "writes a message in the console for debugging and checking work",
-      d: "alien self-destruct button",
-    },
-    correctAnswer: "c",
+    answers: [
+       "solves world hunger",
+       "give the cheat codes to your fav video game",
+       "writes a message in the console for debugging and checking work",
+       "alien self-destruct button",
+    ],
+    correctAnswer: "2",
   },
 ];
 // for(let i=0; i < questionsArray[count].length; i++);
@@ -158,4 +138,4 @@ let questionsArray = [
 //     let questionArray
 // }
 
-startBtn.onclick = quiz;
+startBtn.onclick = countdown;
